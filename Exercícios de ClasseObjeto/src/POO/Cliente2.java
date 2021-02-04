8c650433864a5989249d002b0c9449309e4713c2
Bruna Ariele <bruna.arisoares@gmail.com>
2021-02-03 21:24:41 -0300

    https://github.com/Arileli/-Heran-a.git

---
+++ Exercícios de ClasseObjeto/src/POO/Cliente2.java
@@ -0,0 +1,11 @@
+package POO;
+
+public class Cliente2 {
+
+
+
+		public static void main(String args[]) {
+			Cliente dados = new Cliente ("José", "São Paulo",123);
+			System.out.println(dados.getDados_cliente());
+		}
+	}
